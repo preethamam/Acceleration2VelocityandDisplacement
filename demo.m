d599fc65a247a37b511d102e1ded5e66e72a82cb
Preetham Manjunatha <preethamam@gmail.com>
2021-08-04 05:18:29 -0700

    Update demo.m

--- demo.m
+++ demo.m
@@ -54,8 +54,7 @@ P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 
-% Define the frequency domain f and plot the single-sided amplitude spectrum P1. 
-% The amplitudes are not exactly at 0.7 and 1, as expected, because of the added noise. 
+% Define the frequency domain f and plot the single-sided amplitude spectrum P1.
 % On average, longer signals produce better frequency approximations.
 f = Fs*(0:(L/2))/L;
 
